1c504cb3cf904012cc882cad429eb4169898bc16
DarioDC95 <dario_95@live.com>
2023-03-22 14:59:05 +0100

    migrate orders table done

--- database/migrations/2023_03_22_120253_create_orders_table.php
+++ database/migrations/2023_03_22_120253_create_orders_table.php
@@ -15,7 +15,17 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            
+            $table->foreignId('restaurant_id')
+                  ->references('id')
+                  ->on('restaurants')
+                  ->cascadeOnDelete();
+            $table->string('name_client', 50);
+            $table->string('surname_client', 50);
+            $table->string('email_client', 50);
+            $table->string('phone_client', 20);
+            $table->string('address_client');
+            $table->float('prize', 5, 2);
+            $table->boolean('delivered');
             $table->timestamps();
         });
     }
