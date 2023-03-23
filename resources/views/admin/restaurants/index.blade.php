25ca472d0d1b73a21558b1566cc0db6c18b96155
LucaFaedda <lucakarl91@gmail.com>
2023-03-23 10:11:33 +0100

    creato button

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.admin')
 
 @section('content')
-    QUESTO E' IL TUO RISTORANTE
+
+<div class="container">
+    <h2>Crea il tuo Ristornate</h2>
+    <a class="btn btn-success my-3" href="{{route('admin.create')}}">Aggiungi Ristornate</a>
+</div>
+
 @endsection
