78849f684a525d7a4e0dbbceaa60b471b8df7a37
DarioDC95 <dario_95@live.com>
2023-03-24 17:45:57 +0100

    input register validation from java

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import.meta.glob([
     '../img/**'
 ])
 
+// MODALE BUTTON DELETE
 const deleteButtons = document.querySelectorAll('.confirm-delete-button[type="submit"]')
 
 deleteButtons.forEach((button) => {
@@ -37,3 +38,69 @@ deleteButtons.forEach((button) => {
         })
     })
 });
+
+// CONTROLLO PASSWORD UGUALI
+const registerButton = document.getElementById('register-user-submit');
+
+registerButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    // input
+    const inputName = document.getElementById('name');
+    const inputEmail = document.getElementById('email');
+    const inputPassword = document.getElementById('password');
+    const inputPasswordConfirm = document.getElementById('password-confirm');
+    const inputAddress = document.getElementById('address');
+    const inputPiva = document.getElementById('p_iva');
+    const inputTypes = document.querySelectorAll('.types-checks:checked');
+
+    // errors
+    const errorName = document.getElementById('error-name');
+    const errorEmail = document.getElementById('error-email');
+    const errorPassword = document.getElementById('error-password');
+    const errorAddress = document.getElementById('error-address');
+    const errorPiva = document.getElementById('error-p_iva');
+    const errorTypes = document.getElementById('error-types');
+
+    if (inputName.value.trim() != '' 
+        && inputEmail.value.trim() != '' 
+        && inputPassword.value.trim() == inputPasswordConfirm.value.trim() 
+        && inputPassword.value.trim() != '' 
+        && inputPasswordConfirm.value.trim() != ''
+        && inputAddress.value.trim() != '' 
+        && inputPiva.value.trim() != '') {
+        console.log(inputPassword.value);
+        this.parentElement.parentElement.parentElement.submit();
+        errorName.classList.add('d-none');
+        errorEmail.classList.add('d-none');
+        errorPassword.classList.add('d-none');
+        errorAddress.classList.add('d-none');
+        errorPiva.classList.add('d-none');
+        errorTypes.classList.add('d-none');
+    }
+    else if (inputName.value.trim() == '') {
+        errorName.classList.remove('d-block');
+    }
+    else if (inputName.value.trim() != '') {
+        errorName.classList.add('d-block');
+    }
+    else if (inputEmail.value.trim() == '') {
+        errorEmail.classList.remove('d-none');
+    }
+    else if (inputEmail.value.trim() != '') {
+        errorEmail.classList.add('d-none');
+    }
+    else if (inputPassword.value.trim() == inputPasswordConfirm.value.trim()) {
+        errorPassword.classList.add('d-none');
+    }
+    else if (inputPassword.value.trim() != inputPasswordConfirm.value.trim()) {
+        errorPassword.classList.remove('d-none');
+        console.log(inputTypes);
+    }
+    else if (inputAddress.value.trim() == '') {
+        errorAddress.classList.remove('d-none');
+    }
+    else if (inputAddress.value.trim() != '') {
+        errorAddress.classList.add('d-none');
+    }
+})
