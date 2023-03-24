ebc2811f6cda0d21efc427c6840bfc2b7925c657
DarioDC95 <dario_95@live.com>
2023-03-24 12:22:58 +0100

    modified the registeredController of the user with information of the restaurant adn relative migration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
+use App\Models\Type;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -10,6 +12,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
 
@@ -20,7 +23,9 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        $types = Type::all();
+
+        return view('auth.register', compact('types'));
     }
 
     /**
@@ -34,6 +39,10 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'p_iva' => ['required', 'max:20'],
+            'cover_path' => ['nullable', 'max:65535'],
+            'address' => ['required', 'max:255'],
+            'types' => ['required', 'exists:types,id']
         ]);
 
         $user = User::create([
@@ -41,6 +50,23 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        
+        $img_cover = null;
+        if ($request->has('cover_path')) {
+            $img_cover = Storage::disk('public')->put('cover_path', $request->cover_path);
+        }
+
+
+        $newRestaurant = Restaurant::create([
+            'user_id' => $user->id,
+            'p_iva' => $request->p_iva,
+            'cover_path' => $img_cover,
+            'address' => $request->address
+        ]);
+
+        if ($request->has('types')) {
+            $newRestaurant->types()->attach($request->types);
+        }
 
         event(new Registered($user));
 
