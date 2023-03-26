8397be9f1ab0af4401fc6aac35ee2c3e2c0e15a1
LucaFaedda <lucakarl91@gmail.com>
2023-03-27 01:17:24 +0200

    inserito old

--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -3,11 +3,13 @@
 @section('content')
 
     <div class="col-12">
-        @if ($errors->any())
-            @foreach ($errors->all() as $error)
-                <div class="alert alert-danger">{{ $error }}</div>
-            @endforeach
-        @endif
+        <div class="my-3">
+            @if ($errors->any())
+                @foreach ($errors->all() as $error)
+                    <div class="alert alert-danger">{{ $error }}</div>
+                @endforeach
+            @endif
+        </div>
         <form method="POST" action="{{ route('admin.dishes.update' , $dish->id) }}" enctype="multipart/form-data">
             @csrf
             @method('PUT')
@@ -15,32 +17,31 @@
             {{-- * NOME PIATTO --}}
             <div class="form-group my-2">
                 <label class="fs-2 fw-semibold" for="name">Nome Piatto</label>
-                <input type="text" class="form-control" name="name" id="name" placeholder="Inserire Nome Piatto">
+                <input type="text" class="form-control" name="name" id="name" placeholder="Inserire Nome Piatto" value="{{old('name') ?? $dish->name}}">
             </div>
 
             {{-- * PREZZO PIATTO --}}
             <div class="form-group my-2">
                 <label class="fs-2 fw-semibold" for="name">Prezzo Piatto</label>
-                <input type="number" step=".01" class="form-control" name="price" id="price"
-                    placeholder="Inserire Prezzo Piatto">
+                <input type="number" step=".01" class="form-control" name="price" id="price" placeholder="Inserire Prezzo Piatto" value="{{old('price') ?? $dish->price}}">
             </div>
 
             {{-- * INGREDIENTI --}}
             <div class="form-group my-2">
                 <label class="fs-2 fw-semibold" for="name">Ingredienti</label>
-                <textarea class="form-control" name="ingredients" id="ingredients" placeholder="Inserire Ingredienti"></textarea>
+                <textarea class="form-control" name="ingredients" id="ingredients" placeholder="Inserire Ingredienti" >{{old('ingredients') ?? $dish->ingredients}} </textarea>
             </div>
 
             <div class="form-group my-2">
                 <label class="fs-2 fw-semibold" for="name">Descrizione</label>
-                <textarea class="form-control" name="description" id="description" placeholder="Inserire Descrizione"></textarea>
+                <textarea class="form-control" name="description" id="description" placeholder="Inserire Descrizione">{{old('description') ?? $dish->description}}</textarea>
             </div>
 
             {{-- * VISIBILITA' --}}
             <div class="col-5">
                 <label class="control-label my-2 fw-bold">Visibile</label>
                 <select class="form-control" name="visible" id="visible">
-                    <option value="0">NO</option>
+                    <option value="0" >NO</option>
                     <option value="1" selected>SI</option>
                 </select>
             </div>
