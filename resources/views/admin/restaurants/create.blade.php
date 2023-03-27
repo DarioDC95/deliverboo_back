ee6811577d844b6728fd38b35358a560958b9cc3
DarioDC95 <dario_95@live.com>
2023-03-27 17:18:46 +0200

    create restaurant done

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -9,7 +9,7 @@
                     <div class="alert alert-danger">{{$error}}</div>
                 @endforeach
             @endif
-            <form method="POST" action="{{route('admin.restaurants.store')}}" enctype="multipart/form-data">
+            <form class="myform" method="POST" action="{{route('admin.restaurants.store')}}" enctype="multipart/form-data">
                 @csrf 
 
                 {{-- * PARTITA IVA --}}
@@ -25,7 +25,7 @@
                 </div>
 
                 {{-- * CATEGORIA --}}
-                <div class="form-group my-2">
+                {{-- <div class="form-group my-2">
                     <label class="fs-2 fw-semibold" for="types">Categorie</label>
                     <div>
                         @foreach ($types as $type)
@@ -37,6 +37,35 @@
                         </div>
                         @endforeach
                     </div>
+                </div> --}}
+                <div class="form-group my-2">
+
+                    <label for="types" class="fs-2 fw-semibold">{{ __('Tipologia ') }}</label>
+
+                    <div>
+
+                        <div class="container-select">
+                            <div class="select-btn">
+                                <div class="btn-text">Seleziona Tipologia</div>
+                                <div class="arrow-dwn">
+                                    <i class="fa-solid fa-chevron-down"></i>
+                                </div>
+                            </div>
+                
+                            <ul class="list-items">
+                                @foreach ($types as $type)
+                                <li class="item">
+                                    <input type="checkbox" class="input-checkbox types-checks" value="{{$type->id}}" name="types[]">
+                                    <div class="checkbox">
+                                        <i class="fa-solid fa-check check-icon"></i>
+                                    </div>
+                                    <div class="item-text">{{ $type->name }}</div>
+                                </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        <div id="error-message" style="display: none; color: red;" class="help-block error-help-block">Seleziona almeno una Tipologia</div>
+                    </div>
                 </div>
 
                 {{-- * IMMAGINE --}}
@@ -45,9 +74,18 @@
                     <input class="form-control" type="file"  id="cover_path" name="cover_path">
                 </div>
 
-            <button type="submit" class="btn btn-success">Salva</button>
+                <button type="submit" class="btn btn-success">Salva</button>
             </form>
         </div>
     </div>
 </div>
+
+<!-- Javascript Requirements -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+<!-- Laravel Javascript Validation -->
+<script type="text/javascript" src="{{ asset('vendor/jsvalidation/js/jsvalidation.js')}}"></script>
+
+{!! JsValidator::formRequest('App\Http\Requests\StoreRestaurantRequest') !!}
 @endsection
