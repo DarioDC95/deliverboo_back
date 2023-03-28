04fba52c3b0c766883c400d9bf704c46a1db70d7
LucaFaedda <lucakarl91@gmail.com>
2023-03-28 12:30:39 +0200

    fixed welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 {{-- Home --}}
-{{-- @extends('layouts.app')
+@extends('layouts.app')
 @section('content')
     <div class="container">
         <div class="row flex-column justify-content-center">
@@ -43,24 +43,5 @@
         </div>
     </div>
 
-@endsection --}}
-@extends('layouts.app')
-@section('content')
-<div class="container">
-    <div class="row flex-column justify-content-center">
-        <div class="mt-3 d-flex flex-column text-center col-12">
-            <img style="width:700px" class="img-fluid" src="{{Vite::asset('resources/img/logo-blue.png')}}" alt="">
-            <h2 class="">Diventa un partner di Deliveboo </h2>
-            <p class="fs-5">Insieme possiamo aiutarti a raggiungere sempre più clienti</p>
-        </div>
-        <div class="col-12 text-center">
-            <a  class="btn btn-square btn-info  text-white" href="{{ route('register') }}">
-                Crea un Account
-            </a>
-        </div>
-    </div>
-
-</div>
-
-
 @endsection
+
