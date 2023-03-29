91f2a987de603c03db693bdfa47705404902feb1
DarioDC95 <dario_95@live.com>
2023-03-29 17:51:55 +0200

    api dish done

---
+++ app/Http/Controllers/Api/DishController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Dish;
+use Illuminate\Http\Request;
+
+class DishController extends Controller
+{
+    public function index($id) {
+
+        $dishes = Dish::with('restaurant')->where('restaurant_id', $id)->get();
+
+        if($dishes) {
+            return response()->json([
+                'success' => true,
+                'result' => $dishes
+            ]);
+        }
+        else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Nessun piatto trovato'
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\RestaurantController;
+use App\Http\Controllers\Api\DishController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/restaurants', [RestaurantController::class, 'index']);
+Route::get('/dishes/{id}', [DishController::class, 'index']);
