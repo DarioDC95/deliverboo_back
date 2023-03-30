2b36e3e254c0b6823323dae8d0b4d45aa0fe1503
fedeloxe <loxeee@gmail.com>
2023-03-30 10:39:54 +0200

    seeder dish

--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Dish;
 
 class DishSeeder extends Seeder
 {
@@ -14,6 +15,36 @@ class DishSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $dishes = config('dishes');
+        // $dishes = [
+        //     [
+        //         'restaurant_id' => '7',
+        //         'name' => 'Pizza Margherita',
+        //         'description' => 'La classica pizza margherita',
+        //         'ingredients' => 'Base pizza, passata di pomodoro, mozzarella, basilico, olio extra vergine d\'oliva',
+        //         'price' => 6.50,
+        //         'image_path' => 'image_path/4H2rIPnBoWVbUJacjS4ty1N7piTYItjkJaN2UVdB.jpg',
+        //         'visible' => 1,
+        //         'vegetarian' => 1,
+        //         'vegan' => 0,
+
+        //     ]
+        // ];
+
+        foreach ($dishes as $dish) {
+            $newDish = new Dish();
+            $newDish->restaurant_id = $dish['restaurant_id'];
+            $newDish->name = $dish['name'];
+            $newDish->description = $dish['description'];
+            $newDish->ingredients = $dish['ingredients'];
+            $newDish->price = $dish['price'];
+            $newDish->image_path = $dish['image_path'];
+            $newDish->visible = $dish['visible'];
+            $newDish->vegetarian = $dish['vegetarian'];
+            $newDish->vegan = $dish['vegan'];
+
+
+            $newDish->save();
+        }
     }
 }
