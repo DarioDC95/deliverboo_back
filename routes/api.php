9033276c47593c5169bafcf3f21b56f84fa4859e
DarioDC95 <dario_95@live.com>
2023-03-31 17:52:50 +0200

    chiamata api RestaurantController public function filter

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/restaurants', [RestaurantController::class, 'index']);
+Route::get('/restaurants/{types}', [RestaurantController::class, 'filter']);
 Route::get('/dishes/{id}', [DishController::class, 'index']);
