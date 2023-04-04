a945f1c8af99f58270dd5d2e39583ed7b82a5743
LucaFaedda <lucakarl91@gmail.com>
2023-04-04 11:39:05 +0200

    api order done

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\Api\RestaurantController;
-use App\Http\Controllers\Api\DishController;
-use App\Http\Controllers\Api\TypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\DishController;
+use App\Http\Controllers\Api\TypeController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\RestaurantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,4 @@ Route::get('/restaurants', [RestaurantController::class, 'index']);
 Route::get('/restaurants/{types}', [RestaurantController::class, 'filter']);
 Route::get('/types', [TypeController::class, 'index']);
 Route::get('/dishes/{id}', [DishController::class, 'index']);
+Route::post('/cart', [OrderController::class, 'store']);
