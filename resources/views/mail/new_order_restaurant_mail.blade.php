c43301fce6c58cf89dea5beeae0424f31d7e9d20
DarioDC95 <dario_95@live.com>
2023-04-06 16:02:07 +0200

    mail view for restaurants on order done

--- resources/views/mail/new_order_restaurant_mail.blade.php
+++ resources/views/mail/new_order_restaurant_mail.blade.php
@@ -1,17 +1,22 @@
 <h1>Nuovo Ordine ricevuto da Deliveboo</h1>
 
-<h4>Il seguente contatto:</h4>
+<h3>Il seguente contatto:</h3>
 
-<p>{{ $lead['name_client'] }}</p>
-<p>{{ $lead['surname_client'] }}</p>
-<p>{{ $lead['email_client'] }}</p>
-<p>{{ $lead['phone_client'] }}</p>
-<p>{{ $lead['address_client'] }}</p>
+<p>Nome: {{ $lead['name_client'] }}</p>
+<p>Cognome: {{ $lead['surname_client'] }}</p>
+<p>Email: {{ $lead['email_client'] }}</p>
+<p>Cellulare: {{ $lead['phone_client'] }}</p>
+<p>Indirizzo: {{ $lead['address_client'] }}</p>
 
-<h5>I piatti che ti sono stati ordinati:</h5>
+<h3>ha ordinato i seguenti piatti:</h3>
 
 @foreach ($lead['dishes'] as $item)
-    <p>{{ $item['dish']['name'] }}</p>
+<ul>
+    <li>
+        <span>Piatto: {{ $item['dish']['name'] }}</span>
+        <div>Quantità: {{ $item['quantity'] }}</div>
+    </li>
+</ul>
 @endforeach
 
-<a href="{{ url('http://127.0.0.1:8000/admin/order/' . $lead['order_id']) }}">link al piatto</a>
+<a href="{{ url('http://127.0.0.1:8000/admin/order/' . $lead['order_id']) }}">link all'ordine</a>
